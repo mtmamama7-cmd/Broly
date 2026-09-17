080b6746b4bf9bc4f93c50ac5af4b2f8a68ceab6
mtmamama7-cmd <mtmamama7@gmail.com>
2026-09-17 09:50:52 +0100

    Update uptime.js

--- src/commands/uptime.js
+++ src/commands/uptime.js
@@ -41,7 +41,7 @@ module.exports = {
     const pong = Date.now() - ping;
 
     const lines = [
-      `╔════ DAVID V1 — Status ════╗`,
+      `╔════ 『𝑴𝒂𝒍𝒂𝒌』V1 — Status ════╗`,
       `║ 🤖 Bot ID: ${uid}`,
       `║ ⏱ Uptime: ${formatUptime(upMs)}`,
       `║ 🏓 Ping: ${pong}ms`,
@@ -50,7 +50,7 @@ module.exports = {
       `║ 💻 System RAM: ${((sysM.total-sysM.free)/1073741824).toFixed(2)}/${(sysM.total/1073741824).toFixed(2)} GB`,
       `║ 🛡 Protection: 20 طبقة نشطة`,
       `║ 🔑 Prefix: ${prefix}`,
-      `║ 👑 By: DJAMEL`,
+      `║ 👑 By: Broly`,
       `╚══════════════════════════╝`
     ];
 
